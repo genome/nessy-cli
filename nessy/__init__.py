4221bd2bda78c9d8edf1585f20bc4fea126e88d4
Mark Burnett <mark.m.burnett@gmail.com>
2014-12-30 09:51:47 -0600

    add license

--- nessy/__init__.py
+++ nessy/__init__.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2014 The Genome Institute, Washington University Medical School
+#
+# nessy-cli is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# nessy-cli is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with nessy-cli.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from pprint import pprint
 import argparse
 import logging
